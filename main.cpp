119a305112c448c2176c324700c1a02f8253e381
Michael Frank <153436891+squish-mower@users.noreply.github.com>
2023-12-11 22:57:37 +0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+// Function to check if a number is prime
+bool isPrime(int num) {
+    if (num <= 1) {
+        return false;
+    }
+
+    for (int i = 2; i * i <= num; ++i) {
+        if (num % i == 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+int main() {
+    int number;
+
+    std::cout << "Enter a number: ";
+    std::cin >> number;
+
+    if (isPrime(number)) {
+        std::cout << number << " is a prime number.\n";
+    } else {
+        std::cout << number << " is not a prime number.\n";
+    }
+
+    return 0;
+}
